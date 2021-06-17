930347176579eb5ef2d977a443419753209c273e
“kkiirr201314” <qq255073@gmail.com>
2021-06-17 11:55:18 +0800

    c2-6-1

--- Typeintro.kt
+++ Typeintro.kt
@@ -1,7 +1,7 @@
 fun main(){
-    val playerName: String = "Estradon"
-    playerName = "Madrigl"
+    val playerName: String = "李政龍"
     var experiencePoints: Int = 5
     experiencePoints += 5
     println(experiencePoints)
+    println("玩家姓名:$playerName")
 }
