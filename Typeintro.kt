eadb71a2aa20076fdc4560a1f081e530a3b0a50c
“kkiirr201314” <qq255073@gmail.com>
2021-06-17 14:26:24 +0800

    exec2-10-1

--- Typeintro.kt
+++ Typeintro.kt
@@ -7,10 +7,12 @@ fun main(){
     var numOfgold = 50
     var set = setOf("hydromel","wine","LaCroix sparkling wather")
     experiencePoints += 5
-    println("英雄$playerName"+" 走進一家叫作「獨角獸之角」(「$barName"+"」)的小酒館。")
-    println("酒館老闆$baeBoss"+"問道:  需要牽馬入廄嗎?")
-    println("謝謝,$playerName"+" 回答說，我還沒有馬呢，不過我有$numOfgold"+" 個金幣，先來杯喝的吧。")
-    println("好極了，酒館老闆$baeBoss"+"說，$set"+"，您要哪種?")
+    println("英雄名字$playerName")
+    println("英雄名字${playerName.reversed()}")//文字鏡像
     println("經驗值:"+experiencePoints)
     println("玩家坐騎總數量:"+hasSteed)
+    println("酒館名稱$barName")
+    println("酒館老闆名字$baeBoss")
+    println("玩家金額$numOfgold")
+    println("酒水單$set")
 }
